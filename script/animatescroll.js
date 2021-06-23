9a0f92bdb51fdbf3822eff7fb5a2f09997daa378
donizeti silva <61122214+donizeti26@users.noreply.github.com>
2021-06-23 11:24:05 -0300

    ativando links javascript (não funcionam ainda) adiconando barra de progressso (desenvolvendo) removendo nome do header

---
+++ script/animatescroll.js
@@ -0,0 +1,9 @@
+$('.nav a[href^="#"]').on('click', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href'),
+    targetOffset = $(id).offset().dow;
+
+  $('html, body').animate({
+    scrollTop: targetOffset - 100
+  }, 200);
+});
