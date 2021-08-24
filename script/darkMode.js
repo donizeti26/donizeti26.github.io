5b3d2fab47e456aae1574711debe56644e8eaf84
donizeti silva <61122214+donizeti26@users.noreply.github.com>
2021-08-24 10:45:14 -0300

    corrigindo bugs tema escuro

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,23 +1,24 @@
 function onClickSliderRound() {
   console.log("CERTO");
   const wallpaper = document.getElementsByClassName("page-lading");
-  wallpaper[0].classList.toggle('dark-mode')
-  console.log(wallpaper)
+  wallpaper[0].classList.toggle('dark-mode');
+  console.log(wallpaper);
 
-  const spa = document.getElementById('titleleaked')
-  spa.classList.toggle('dark-mode-title')
+  const spa = document.getElementById('titleleaked');
+  spa.classList.toggle('dark-mode-title');
 
-  const body = document.getElementById('body')
-  body.classList.toggle('grey-mode')
+  const body = document.getElementById('body');
+  body.classList.toggle('grey-mode');
 
-  const texto = document.querySelectorAll('p')
+  const rodape = document.getElementsByClassName('rodape');
+  rodape[0].classList.toggle('dark-mode');
 
-  for (var i = 0; i <= texto.length; i++) {
-    texto[i].classList.add('dark-mode-title-white')//fazer um for!
-  }
-  const divs = document.querySelectorAll('div')
 
-  divs.map(divs.classList.add('dark-mode-title-white'))
+  const variavelp = document.querySelectorAll('p,h4,h1,li');
+  for (var i = 0; i < variavelp.length; i++) {
+    var item = variavelp[i];  // Calling myNodeList.item(i) isn't necessary in JavaScript
+    item.classList.toggle('dark-mode-title-white');
+  };
 
 
 
